7cc6b97c68537209f03bf33e8394f1a5b52eaa7f
April Copley <52804777+acopperlily@users.noreply.github.com>
2022-10-15 05:03:27 -0400

    Update index-START.js

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,37 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
+// function reverseString(text) {
+//   return text.split('').reverse().join('');
+// }
 
+// const reverseString = text => [...text].reverse().join('');
 
-function reverseString(text) {
-    // Code goes here
-}
+// function reverseString(text) {
+//   let result = '';
+//   for (let i = text.length - 1; i >= 0; i--)
+//     result += text[i];
+//   return result;
+// }
 
+// const reverseString = text => {
+//   let result = '';
+//   for (let char of text)
+//     result = char + result;
+//   return result;
+// };
 
+// function reverseString(text) {
+//   if (text === '') return '';
+//   return reverseString(text.substr(1)) + text[0];
+// }
 
-module.exports = reverseString
+// const reverseString = text => text === '' ? '' : reverseString(text.slice(1)) + text[0];
+
+// function reverseString(text) {
+//   return text.split('').reduce((acc, char) => char + acc, '');
+// }
+
+const reverseString = text => [...text].reduce((acc, c) => c + acc, '');
+
+module.exports = reverseString;
