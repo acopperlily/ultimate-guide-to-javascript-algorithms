94a98f60d11337e2c845a093a9dcf8e47edd14a8
acopperlily <acopperlily@yahoo.com>
2022-10-16 04:14:36 -0400

    Solve maxRecurringChar

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -5,10 +5,62 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
-function maxRecurringChar(text) {
-    // Code goes here
-}
+// function maxRecurringChar(text) {
+//   let charMap = {};
+//   let maxCharValue = 0;
+//   let maxChar = '';
 
+//   for (let char of text) {
+//     if (charMap.hasOwnProperty(char)) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > maxCharValue) {
+//       maxCharValue = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+
+//   return maxChar;
+// }
+
+// function maxRecurringChar(text) {
+//   let charMap = {};
+//   let charArray = [];
+//   let valuesArray = [];
+//   let maxCharValue = 0;
+
+//   for (let char of text) {
+//     if (charMap.hasOwnProperty(char)) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+
+//   charArray = Object.keys(charMap);
+//   valuesArray = Object.values(charMap);
+//   maxCharValue = Math.max(...valuesArray);
+
+//   return charArray[valuesArray.indexOf(maxCharValue)];
+// }
+
+const maxRecurringChar = text => {
+  let charMap = {};
+
+  for (let char of text)
+    charMap.hasOwnProperty(char) ? charMap[char]++ : charMap[char] = 1;
+
+  const charArray = Object.keys(charMap);
+  const valuesArray = Object.values(charMap);
+  const maxCharValue = Math.max(...valuesArray);
+
+  return charArray[valuesArray.indexOf(maxCharValue)];
+};
 
 
 module.exports = maxRecurringChar;
