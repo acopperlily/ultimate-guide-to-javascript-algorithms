8123338dc9bc0398903aceac115906f3f24e770d
acopperlily <acopperlily@yahoo.com>
2022-10-23 03:44:45 -0400

    Solve longestWord

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -5,9 +5,51 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 
-function longestWord(text) {
-    // Code goes here
-}
+// function longestWord(text) {
+//   const wordArray = text.split(' ');
+//   let maxLength = 0;
+//   let result = '';
 
+//   for (let i = 0; i < wordArray.length; i++) {
+//     if (wordArray[i].length > maxLength) {
+//       maxLength = wordArray[i].length;
+//       result = wordArray[i];
+//     }
+//   }
+//   return result;
+// }
+
+// function longestWord(text) {
+//   const wordArray = text.split(' ');
+//   let result = '';
+
+//   for (let word of wordArray) {
+//     if (word.length > result.length)
+//       result = word;
+//   }
+//   return result;
+// }
+
+// function longestWord(text) {
+//   let result = text.split(' ').reduce((maxLengthWord, currentWord) => {
+//     if (currentWord.length > maxLengthWord.length) {
+//       return currentWord;
+//     } else {
+//       return maxLengthWord;
+//     }
+//   }, '');
+//   return result;
+// }
+
+// const longestWord = text => text.split(' ').reduce((maxLengthWord, currentWord) => currentWord.length > maxLengthWord.length ? currentWord : maxLengthWord, '');
+
+// function longestWord(text) {
+//   const sortedArray = text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length);
+//   return sortedArray[0];
+// }
+
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord('Top Shelf Web Development Training on Scotch'));
 
 module.exports = longestWord
