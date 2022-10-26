ac3d38a68364e227c681876d5ee5fd3d7b2c5383
acopperlily <acopperlily@yahoo.com>
2022-10-26 08:43:11 -0400

    Solve mergeArrays

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,38 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
+//   arrays.forEach(array => {
+//     jointArray = [...jointArray, ...array];
+//   });
+//   return [...new Set([...jointArray])];
+// }
+
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
+//   arrays.forEach(array => {
+//     jointArray = [...jointArray, ...array];
+//   });
+//   const uniqueArray = jointArray.filter((item, index) => jointArray.indexOf(item) === index);
+//   return uniqueArray;
+// }
+
 function mergeArrays(...arrays) {
-
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
-
-    return [...new Set([...jointArray])]
-
-    
+  let jointArray = [];
+  arrays.forEach(array => {
+    jointArray = [...jointArray, ...array];
+  });
+  const uniqueArray = jointArray.reduce((newArray, item) => {
+    if (newArray.includes(item)) {
+      return newArray;
+    } else {
+      return [...newArray, item];
+    }
+  }, []);
+  return uniqueArray;
 }
 
+console.log(mergeArrays([1, 2, 3, 3, 3], [1, 4, 5, 2]), [1, 2, 3, 4, 5]);
 
 module.exports = mergeArrays
