4a5fe1ce93982172201e4a321a38d8164dc03aab
acopperlily <acopperlily@yahoo.com>
2022-10-26 12:21:37 -0400

    Solve falsyBouncer

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -6,9 +6,20 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
 
-function falsyBouncer(array) {
-    // Code goes here
-}
+// function falsyBouncer(arr) {
+//   let result = [];
+//   for (let value of arr) {
+//     if (value) result.push(value);
+//   }
+//   return result;
+// }
 
+// function falsyBouncer(arr) {
+//   return arr.filter(value => Boolean(value));
+// }
+
+const falsyBouncer = arr => arr.filter(value => !!value);
+
+console.log(falsyBouncer([1, 0, null, '', 5]), [1, 5]);
 
 module.exports = falsyBouncer
