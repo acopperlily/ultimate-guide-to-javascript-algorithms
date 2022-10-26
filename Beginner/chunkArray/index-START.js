9fbea9b004521c18974fdd2577a9fed557a99524
acopperlily <acopperlily@yahoo.com>
2022-10-26 05:35:17 -0400

    Solve chunkArray

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,12 +1,40 @@
-/* 
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
- E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
-*/
+// function chunkArray(arr, size) {
+//   let result = [];
+//   for (let value of arr) {
+//     let lastArray = result[result.length - 1];
+//     if (!lastArray || lastArray.length === size) {
+//       result.push([value]);
+//     } else {
+//       lastArray.push(value);
+//     }
+//   }
+//   return result;
+// }
 
-function chunkArray(array, size) {
-    // Code goes here
+// function chunkArray(arr, size) {
+//   let result = [];
+//   let arrCopy = [...arr];
+//   while (arrCopy.length > 0) {
+//     result.push(arrCopy.splice(0, size));
+//   }
+//   return result;
+// }
+
+// function chunkArray(arr, size) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i += size) {
+//     let chunk = arr.slice(i, i + size);
+//     result.push(chunk);
+//   }
+//   return result;
+// }
+
+function chunkArray(arr, size) {
+  if (arr.length <= size) return [arr];
+  return [arr.slice(0, size), ...chunkArray(arr.slice(size), size)];
 }
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
 
 
 module.exports = chunkArray
