1dabe95e75b1a9d7e2e4772cc35fc6fb25571b28
acopperlily <acopperlily@yahoo.com>
2022-10-27 03:31:07 -0400

    Solve whereIBelong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -9,11 +9,39 @@ than 1(index 0), but less than 2(index 1).
 
 
 
-function whereIBelong(arr, num) {
-   // Code goes here
+// function whereIBelong(arr, num) {
+//   arr.sort((a, b) => a - b);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= num) return i;
+//   }
+//   return arr.length;
+// }
 
-}
+// function whereIBelong(arr, num) {
+//   let counter = 0;
+//   for (let n of arr) {
+//     if (num > n) counter++;
+//   }
+//   return counter;
+// }
 
+// function whereIBelong(arr, num) {
+//   arr.sort((a, b) => a - b);
+//   let counter = 0;
+//   while (num > arr[counter]) counter++;
+//   return counter;
+// }
 
+// function whereIBelong(arr, num) {
+//   arr.push(num);
+//   arr.sort((a, b) => a - b);
+//   return arr.indexOf(num);
+// }
+
+// const whereIBelong = (arr, num) => arr.filter(n => n < num).length;
+
+const whereIBelong = (arr, num) => arr.reduce((acc, c) => c < num ? acc + 1 : acc, 0);
+
+console.log(whereIBelong([1, 2, 3, 4], 1.5), 1);
 
 module.exports = whereIBelong
