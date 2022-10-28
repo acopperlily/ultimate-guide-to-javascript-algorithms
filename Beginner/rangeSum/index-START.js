20e5ae98183a77cc72c888ed5ab9157ec7ca691f
acopperlily <acopperlily@yahoo.com>
2022-10-28 03:02:23 -0400

    Solve rangeSum

--- Beginner/rangeSum/index-START.js
+++ Beginner/rangeSum/index-START.js
@@ -1,5 +1,28 @@
-function rangeSum(arr) {
+// function rangeSum(arr) {
+//   let sum = 0;
+//   for (i = arr[0]; i <= arr[1]; i++)
+//     sum += i;
+//   return sum;
+// }
 
+// function rangeSum(arr) {
+//   return ((arr[1] - arr[0] + 1) * (arr[0] + arr[1])) / 2;
+// }
+
+// function rangeSum(arr) {
+//   if (arr[0] === arr[1])
+//     return arr[0];
+//   else
+//     return rangeSum([arr[0], arr[1] - 1]) + arr[1];
+// }
+
+function rangeSum(arr) {
+  const arrList = [];
+  for (i = arr[0]; i <= arr[1]; i++)
+    arrList.push(i);
+  return arrList.reduce((acc, num) => acc + num, 0);
 }
+
+console.log(rangeSum([1, 9]));
 
 module.exports = rangeSum;
--- Beginner/rangeSum/test.js
+++ Beginner/rangeSum/test.js
@@ -1,1 +1,9 @@
 const rangeSum = require('./index-START');
+
+test('rangeSum is a function', () => {
+  expect(typeof rangeSum).toEqual('function');
+});
+
+test('rangeSum adds positive nums', () => {
+  expect(rangeSum([1, 9])).toEqual(45);
+});
